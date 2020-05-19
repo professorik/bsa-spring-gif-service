1c6533e5375404cec848727bf1b1d02dd18ed788
Pavel Nemchenko <pavelnemoi@gmail.com>
2020-05-19 19:47:11 +0300

    Initial

---
+++ src/main/java/com/bsa/boot/BootApplication.java
@@ -0,0 +1,14 @@
+package com.bsa.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+@SpringBootApplication
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+
+}
