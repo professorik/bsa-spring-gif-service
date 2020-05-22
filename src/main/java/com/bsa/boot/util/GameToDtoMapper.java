09312667326861715d6dc8e82b6a6113c04faed9
Pavel Nemchenko <pavelnemoi@gmail.com>
2020-05-22 23:14:03 +0300

    Refactor

--- src/main/java/com/bsa/boot/util/GameToDtoMapper.java
+++ src/main/java/com/bsa/boot/util/GameToDtoMapper.java
@@ -2,12 +2,12 @@ package com.bsa.boot.util;
 
 import com.bsa.boot.dto.GameDto;
 import com.bsa.boot.entity.Game;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
+@Component
 public final class GameToDtoMapper {
     public GameDto map(Game game) {
         return new GameDto(
